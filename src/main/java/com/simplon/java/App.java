175c68f526a375bf97d6d3e239047c3529aa8ef5
Marie Baude <33251784+MarieBaude@users.noreply.github.com>
2022-12-08 14:48:08 +0100

    adding connexion to bdd

--- src/main/java/com/simplon/java/App.java
+++ src/main/java/com/simplon/java/App.java
@@ -1,5 +1,7 @@
 package com.simplon.java;
 
+import java.sql.*;
+
 /**
  * Hello world!
  *
@@ -8,6 +10,25 @@ public class App
 {
     public static void main( String[] args )
     {
-        System.out.println( "Hello World!" );
+        String url = "jdbc:postgresql://localhost:5432/entreprise";
+        String user = "yassen";
+        String pass = "azerty";
+        
+        try {
+        	Class.forName("org.postgresql.Driver");
+        	Connection conn = DriverManager.getConnection(url, user, pass);
+        	
+        	Statement stat = conn.createStatement();
+        	
+        	ResultSet result = stat.executeQuery("SELECT * FROM emp");
+        	
+        	while (result.next()) {
+        		System.out.println(result.getString("nom"));
+        	}
+        	
+        	
+        } catch (Exception e) {
+        	e.printStackTrace();
+        } 
     }
 }
